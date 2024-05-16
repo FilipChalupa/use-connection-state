f725470f22f6532b20fe24c3504ffc17c08bfacf
Filip Chalupa <chalupa.filip@gmail.com>
2024-05-16 14:42:18 +0200

    Rewrite to `useSyncExternalStore`

--- src/createUseConnectionStateHook.ts
+++ src/createUseConnectionStateHook.ts
@@ -1,26 +1,22 @@
 import { ConnectionOptions, connectionState } from 'connection-state'
-import { useEffect, useState } from 'react'
+import { useCallback, useSyncExternalStore } from 'react'
 
 export const createUseConnectionStateHook = (options?: ConnectionOptions) => {
 	const connection = connectionState(options)
 	const getState = () => connection.getState()
+	const initialState = getState()
 
 	const useConnectionState = () => {
-		const [state, setState] = useState(() => getState())
-
-		useEffect(() => {
-			const refresh = () => {
-				setState(getState())
-			}
-			refresh()
-
-			connection.addListener(refresh)
+		const subscribe = useCallback((onStoreChange: () => void) => {
+			connection.addListener(onStoreChange)
 			return () => {
-				connection.removeListener(refresh)
+				connection.removeListener(onStoreChange)
 			}
 		}, [])
+		const getSnapshot = useCallback(() => getState(), [])
+		const getServerSnapshot = useCallback(() => initialState, [])
 
-		return state
+		return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
 	}
 
 	return useConnectionState
